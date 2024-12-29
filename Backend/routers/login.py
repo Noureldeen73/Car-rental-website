2afa77665c504e372539e218acfd3a59d5ae83c9
Eiad-Alaa <eiad.alaa.nouh@gmail.com>
2024-12-29 22:03:57 +0200

    Fix password hash

--- Backend/routers/login.py
+++ Backend/routers/login.py
@@ -17,8 +17,8 @@ async def authenticateLoginDetails(email: str, password: str, db=Depends(get_db)
     # if exists, return {user_id: user_id, is_admin: is_admin}
     hashed_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
     hashed_password_str = hashed_password.decode('utf-8')
-    user = await db.fetchrow("""SELECT * FROM "User" WHERE email = $1 AND password = $2""", email, hashed_password_str)
-    if user:
+    user = await db.fetchrow("""SELECT * FROM "User" WHERE email = $1""", email)
+    if user and bcrypt.checkpw(password.encode('utf-8'), user['password'].encode('utf-8')):
         return {"user_id": user['user_id'], "is_admin": user['is_admin']}
     else:
         raise HTTPException(status_code=400, detail="Invalid email or password")
