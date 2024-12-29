f26cdf529251151d9910cf174e74109cd7c68f0f
Eiad-Alaa <eiad.alaa.nouh@gmail.com>
2024-12-29 22:05:41 +0200

    Add admin endpoint

--- Backend/routers/admin.py
+++ Backend/routers/admin.py
@@ -20,12 +20,21 @@ async def get_all_reservations(db=fastapi.Depends(get_db)):
 
 
 @router.get('Admin/get_reservations_by_car/')
-async def get_reservations_by_car(car_id: int, db=fastapi.Depends(get_db)):
+async def get_reservations_by_car(plat_id: str, db=fastapi.Depends(get_db)):
     try:
-        reservations = await db.fetch("""SELECT * FROM Reservation WHERE car_id = $1""", car_id)
+        reservations = await db.fetch("""SELECT * FROM Reservation WHERE plat_id = $1""", plat_id)
         if not reservations:
             raise fastapi.HTTPException(status_code=404, detail="No reservations found")
         return [dict(reservation) for reservation in reservations]  # Convert asyncpg record to dictionary
     except Exception as e:
         raise fastapi.HTTPException(status_code=500, detail=str(e))
 
+@router.get('Admin/get_status_cars/')
+async def get_status_cars(db=fastapi.Depends(get_db)):
+    try:
+        cars = await db.fetch("""SELECT plat_id, available FROM Car WHERE available = True""")
+        if not cars:
+            raise fastapi.HTTPException(status_code=404, detail="No cars found")
+        return [dict(car) for car in cars]  # Convert asyncpg record to dictionary
+    except Exception as e:
+        raise fastapi.HTTPException(status_code=500, detail=str(e))
