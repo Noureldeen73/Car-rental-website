dfe4c961af08e2871e7b6861af5fc00164136558
Mohamed Khaled <mohamed.khaled1872002@gmail.com>
2024-12-30 03:18:21 +0200

    Handle extra ceses in get reservation

--- Backend/routers/car.py
+++ Backend/routers/car.py
@@ -13,11 +13,28 @@ router = fastapi.APIRouter(
 @router.get('/get_car_by_filter/')
 async def get_all_reservations(model: str, year:int, city:str, db=fastapi.Depends(get_db)):
     try:
-        cars = await db.fetch("""SELECT * FROM Car WHERE model = $1 AND year = $2 AND 
+        if model == '' and year == 0 and city == '':
+            cars = []
+        elif model == '' and year == 0:
+            cars = await db.fetch("""SELECT * FROM Car WHERE office_id IN (SELECT office_id FROM Office WHERE city = $3)""", city)
+        elif model == '' and city == '':
+            cars = await db.fetch("""SELECT * FROM Car WHERE year = $2""", year)
+        elif year == 0 and city == '':
+            cars = await db.fetch("""SELECT * FROM Car WHERE model = $1""", model)
+        elif year == 0:
+            cars = await db.fetch("""SELECT * FROM Car WHERE model = $1 AND 
+        office_id IN (SELECT office_id FROM Office WHERE city = $3)""", model, city)
+        elif city == '':
+            cars = await db.fetch("""SELECT * FROM Car WHERE model = $1 AND year = $2""", model, year)
+        elif model == '':
+            cars = await db.fetch("""SELECT * FROM Car WHERE year = $2 AND 
+        office_id IN (SELECT office_id FROM Office WHERE city = $3)""", year, city)
+        else:
+            cars = await db.fetch("""SELECT * FROM Car WHERE model = $1 AND year = $2 AND 
         office_id IN (SELECT office_id FROM Office WHERE city = $3)""", model, year, city)
         if not cars:
             raise fastapi.HTTPException(status_code=404, detail="No cars found")
-        return [dict(car) for car in cars]  # Convert asyncpg record to dictionary
+        return [dict(car) for car in cars]
 
     except Exception as e:
         raise fastapi.HTTPException(status_code=500, detail=str(e))
